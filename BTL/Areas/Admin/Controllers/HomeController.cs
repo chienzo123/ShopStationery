fd9b67cd99cead6a04ea20fdc2f1078dc85c7b26
Chiến <chienop298@gmail.com>
2021-07-20 13:59:03 +0700

    Chien commit

---
+++ BTL/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Caterogy()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+        public ActionResult Card()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ BTL/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()//Trang chủ
+        {
+            return View();
+        }
+
+        public ActionResult Details()//Chi tiết
+        {
+          
+
+            return View();
+        }
+
+        public ActionResult Contact()//Liên hệ
+        {
+           
+
+            return View();
+        }
+        public ActionResult Caterogy()//Danh Mục
+        {
+            return View();
+        }
+        public ActionResult Oder()//Đặt Hàng
+        {
+            return View();
+        }
+        public ActionResult Card()//Giỏ Hàng
+        {
+            return View();
+        }
+        public ActionResult Register()//Đăng Ký
+        {
+            return View();
+        }
+        public ActionResult Login()//Đăng nhập
+        {
+            return View();
+        }
+        public ActionResult Acccount()//Tài Khoản
+        {
+            return View();
+        }
+
+
+
+    }
+}
